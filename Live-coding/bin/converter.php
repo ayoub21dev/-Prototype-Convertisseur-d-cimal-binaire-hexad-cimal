ac56c9dadd1787ab385ecde92b227847d0b8cc1f
ayoub21dev <Ayoubcode941@gmail.com>
2025-09-24 13:35:18 +0100

    Done

--- Live-coding/bin/converter.php
+++ Live-coding/bin/converter.php
@@ -3,66 +3,46 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\NumberConverter;
+use InvalidArgumentException;
+use Throwable;
 
-try{
-
-    $number = null;
-    $options = [];
+try {
+    $options = getopt('', [
+        'and:', 'or:', 'xor:', 'not', 'shift-left:', 'shift-right:',
+    ]);
     
-    for($i = 1; $i < count($argv); $i++) {
-        $arg = $argv[$i];
-        
-        if(str_starts_with($arg, '--')) {
-            if(str_contains($arg, '=')) {
-                [$option, $value] = explode('=', substr($arg, 2), 2);
-                $options[$option] = $value;
-            } else {
-                $option = substr($arg, 2);
-                $options[$option] = true;
-            }
-        } else if($number === null && is_numeric($arg)) {
-            $number = $arg;
-        }
+    $number = (int) ($argv[1] ?? null);
+
+    if (!is_numeric($number) || empty($number)) {
+        throw new InvalidArgumentException("Veuillez fournir un nombre valide.");
     }
 
-    if($number === null || !is_numeric($number)){
-        throw new InvalidArgumentException("Please Provide A Valid Integer");
-    }
+    $converter = new NumberConverter($number);
+    
+    echo $converter->format("Decimal", $converter->toDecimal());
+    echo $converter->format("Binary", $converter->toBinary());
+    echo $converter->format("Hexa", $converter->toHexa());
 
-    $converter = new NumberConverter((int)$number);
-
-    echo $converter->format("Decimal" , $converter->toDecimal());
-    echo $converter->format("Binary" , $converter->toBinary());
-    echo $converter->format("Hexa" , $converter->toHexa());
-
-
-    if(isset($options['and'])){
+    if (isset($options['and'])) {
         echo $converter->format("AND", $converter->BitwiseAnd((int)$options['and']));
     }
-
-    if(isset($options['or'])){
+    if (isset($options['or'])) {
         echo $converter->format("OR", $converter->BitwiseOr((int)$options['or']));
     }
-
-    if (isset($options['xor'])){
+    if (isset($options['xor'])) {
         echo $converter->format("XOR", $converter->BitwiseXor((int)$options['xor']));
     }
-
-    if (isset($options['not'])){
+    if (isset($options['not'])) {
         echo $converter->format("NOT", $converter->BitwiseNot());
     }
-
-    if (isset($options['shift-left'])){
+    if (isset($options['shift-left'])) {
         echo $converter->format("Shift Left", $converter->ShiftLeft((int)$options['shift-left']));
     }
-
-    if (isset($options['shift-right'])){
+    if (isset($options['shift-right'])) {
         echo $converter->format("Shift Right", $converter->ShiftRight((int)$options['shift-right']));
     }
 
-
-}
-catch(Throwable $e){
-    fwrite(STDERR, "Error: " . $e->getMessage() . PHP_EOL);
+} catch (Throwable $e) {
+    fwrite(STDERR, "Erreur: " . $e->getMessage() . PHP_EOL);
     exit(1);
 }
