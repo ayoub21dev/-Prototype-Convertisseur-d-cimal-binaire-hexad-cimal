15f72151ed46eebf4b82332f530db86fd719d396
ayoub21dev <Ayoubcode941@gmail.com>
2025-09-25 10:49:50 +0100

    added classes

---
+++ Réalisation/src/Calculator.php
@@ -0,0 +1,35 @@
+<?php
+namespace APP;
+class calculator{
+ private int $numberA;
+ private int $numberB;
+    public function __construct(int $number1,int $number2)
+    {
+        $this->numberA = $number1;
+        $this->numberB = $number2;
+    }
+
+   public function and(): int
+    {
+        return $this->numberA & $this->numberB;
+    }
+
+    public function or(): int
+    {
+        return $this->numberA | $this->numberB;
+    }
+
+    public function xor(): int
+    {
+        return $this->numberA ^ $this->numberB;
+    }
+
+    public function notA(): int
+    {
+        return ~$this->numberA;
+    }
+
+
+
+
+}
--- Réalisation/src/Converter.php
+++ Réalisation/src/Converter.php
@@ -1,9 +1,18 @@
 <?php 
+
 namespace APP;
-
 class Converter{
+            private int $number;
+            public function __construct(int $number)
+                                                {
+               $this->number = $number;
+             }
+             public function toBinary(){
+                return decbin($this->number);
+             }
+             public function toHexa(){
+                return dechex($this->number);
+             }
+  
 
-
-
-    
 }
